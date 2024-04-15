0921c99d03d1b26dd8d8c8bceb693f40965bf86e
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-04-15 14:33:46 +0200

    Create MenuItem.cs

---
+++ Logic/Pages/MenuItem.cs
@@ -0,0 +1,12 @@
+public class MenuItem{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Category { get; set; }
+
+    public MenuItem(string name, double price, string category){
+        Name = name;
+        Price = price;
+        Category = category;
+    }
+//CHANGE CATEGORY
+}
