8c40bfe5da81f5920fc265e2c304c28ac22bb4f7
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-04-15 14:33:13 +0200

    Update Menu.cs

--- Logic/Pages/Menu.cs
+++ Logic/Pages/Menu.cs
@@ -1,17 +1,49 @@
-public class Menu : Page{
+public class Menu : Page
+{
     private string _name = "Menu";
+    public List<MenuItem> Items { get; set; }
 
-    public override string Name
+    public override string Name => _name;
+
+    public Menu()
     {
-        get => _name;
+        Items = new List<MenuItem>
+        {
+            new MenuItem("Salmon", 15.99, "Fish"),
+            new MenuItem("Steak", 12.99, "Meat"),
+            new MenuItem("Caesar Salad", 8.99, "Vegetarian"),
+            new MenuItem("Soda", 2.49, "Drink"),
+            new MenuItem("Iced Tea", 1.99, "Drink") 
+            //CHANGE CATEGORY AND MAKE INTO JSON
+        };
     }
+
+    public void AddItem()
+    {
+    }
+
+    public void RemoveItem()
+    {
+
+    }
+
+    public void ChangeItem() 
+    {
+    }
+
+    public void DisplayMenu()
+    {
+    }
+
     public override void Options()
     {
-        System.Console.WriteLine("add more options here"); //view menu if logged in as customer
+        Console.WriteLine("[V] View menu");
+        Console.WriteLine("[VC] View a specific category");
         base.Options();
+        //READLINE AND MAKE IT WORK
     }
+
     public override void Contents()
     {
-        
     }
 }
