8837646d553a4657a2063691f8d8171b6d842f68
Githubjael <jaelimamdi@gmail.com>
2024-04-17 12:53:34 +0200

    dit zal niet gebruikt worden.

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
-public class Program
-{
-    public static void Main()
-    {
-        // H = Homepage
-        // M = Menu
-        // R = Reservation
-        // RV = Review 
-        // C = Contact
-        // E = Exit
-        //create all pages
-        Page homePage = new Home();
-        Page ContactPage = new Contact();
-        Page ReservationPage = new Reservation();
+// public class Program
+// {
+//     public static void Main()
+//     {
+//         // H = Homepage
+//         // M = Menu
+//         // R = Reservation
+//         // RV = Review 
+//         // C = Contact
+//         // E = Exit
+//         //create all pages
+//         Page homePage = new Home();
+//         Page ContactPage = new Contact();
+//         Page ReservationPage = new Reservation();
         
-        bool Loop = true; // loop bool
-        do{
-            //begin program
-            homePage.Options();
-            var UserInput = Console.ReadLine();
-            if (UserInput.ToLower() == "e" || UserInput.ToLower() == "exit"){
-                Loop = false;
-            }
-        } while(Loop);
-    }
-}
+//         bool Loop = true; // loop bool
+//         do{
+//             //begin program
+//             homePage.Options();
+//             var UserInput = Console.ReadLine();
+//             if (UserInput.ToLower() == "e" || UserInput.ToLower() == "exit"){
+//                 Loop = false;
+//             }
+//         } while(Loop);
+//     }
+// }
