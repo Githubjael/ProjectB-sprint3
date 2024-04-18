8e0f5414f2fcfe353993b34ea64d42f36e5d6776
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-04-18 16:26:48 +0200

    Update Table.cs

--- Logic/Furniture/Table.cs
+++ Logic/Furniture/Table.cs
@@ -3,22 +3,17 @@ public class Table
     // zeker nodig
     public string ID { get; set; }
     public bool Reserved { get; set; }
-    public int Type { get; set; }
+    public virtual int Type { get; set; }
     // niet zeker
     // public int AmountOfChairs { get; set; }
     // gewone constructor
-    public Table(int id, int type) // int id is een int, om het makkelijk om tafels te instantiaten
+    public Table(string id) // int id is een int, om het makkelijk om tafels te instantiaten
     {
         ID = $"{id}";
         Reserved = false;
     }
     // als we een tafel moeten maken dat al reserved is? niet zeker
-    public Table(int id, bool reserved)
-    {
-        ID = $"{id}";
-        Reserved = reserved;
-    }
-    public void IsReserved()
+        public void IsReserved()
     {
         Reserved = true;
     }
