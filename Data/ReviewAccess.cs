adf520cf68a6ce1da3a174432b0e2a51f9640132
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-21 22:50:30 +0200

    Update ReviewAccess.cs

--- Data/ReviewAccess.cs
+++ Data/ReviewAccess.cs
@@ -1,40 +1,41 @@
-// using Newtonsoft.Json;
+using Newtonsoft.Json;
 
-// class ReviewAccess
-// {
-//     private static string fileName = @"Logic\Pages\Reviews.cs";
+class ReviewAccess
+{
+    private static string fileName = @"C:\Users\Hilal\OneDrive\Bureaublad\imdone\ProjectB-sprint3-main\DataSources\Reviews.Json"; //vergeet niet je eigen path te copy
 
-//     public static List<T> ReadFromJson()
-//     {
-//         try
-//         {
-//             using (StreamReader reader = new StreamReader(fileName))
-//             {
-//                 string json = reader.ReadToEnd();
-//                 return JsonConvert.DeserializeObject<List<Review>>(json);
-//             }
-//         }
-//         catch (FileNotFoundException)
-//         {
-//             Console.WriteLine("no reviews available");
-//             return new List<Review>();
-//         }
-//     }
+    public static List<Review> ReadFromJson()
+    {
+        try
+        {
+            using (StreamReader reader = new StreamReader(fileName))
+            {
+                string json = reader.ReadToEnd();
+                List<Review> allreviews = JsonConvert.DeserializeObject<List<Review>>(json);
+                return allreviews;
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("no json file found");
+            return new List<Review>();
+        }
+    }
 
-//     public static void WriteToJson(List<T> reviews)
-//     {
-//         try
-//         {
-//             using (StreamWriter writer = new StreamWriter(fileName))
-//             {
-//                 string json = JsonConvert.SerializeObject(reviews);
-//                 writer.Write(json);
-//             }
-//         }
-//         catch (IOException ex)
-//         {
-//             Console.WriteLine($"Error writing reviews file: {ex.Message}");
-//             throw;
-//         }
-//     }
-// }
+    public static void WriteToJson(List<Review> reviews)
+    {
+        try
+        {
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                string json = JsonConvert.SerializeObject(reviews);
+                writer.Write(json);
+            }
+        }
+        catch (IOException ex)
+        {
+            Console.WriteLine($"Error writing reviews file: {ex.Message}");
+            throw;
+        }
+    }
+}
