108be1b89bd8f62698a3b8834613dfa5c0c7a1c7
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-21 22:52:59 +0200

    Update Review.cs

--- Logic/Review.cs
+++ Logic/Review.cs
@@ -1,10 +1,10 @@
 public class Review
 {
     public string GuestName { get; set; }
-    public int Rating { get; set; }
+    public string Rating { get; set; }
     public string Comments { get; set; }
 
-    public Review(string guestName, int rating, string comments)
+    public Review(string guestName, string rating, string comments)
     {
         GuestName = guestName;
         Rating = rating;
