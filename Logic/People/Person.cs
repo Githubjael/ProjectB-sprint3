2c27dba5b08bf872f2fc6a55ed5c7838e1baf707
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-22 15:40:32 +0200

    Update Person.cs

--- Logic/People/Person.cs
+++ Logic/People/Person.cs
@@ -1,14 +1,17 @@
-class Person{
-    // Every person of ze nu employee of gast zijn hebben een voor- en achternaam (obviously) en ze hebben een email en telefoonr
-    public string FirstName {get; set;}
-    public string LastName {get; set;}
-    public string EmailAddress {get; set;}
-    public string PhoneNumber {get; set;}
-    public Person(string firstName, string lastName, string emailAddress, string phoneNumber)
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+    public string phoneNumber { get; set; }
+    public string Password { get; set; }
+
+    public Person(string firstName, string lastName, string emailAddress, string phonenumber, string password)
     {
         FirstName = firstName;
         LastName = lastName;
         EmailAddress = emailAddress;
-        PhoneNumber = phoneNumber;
+        phoneNumber = phonenumber;
+        Password = password;
     }
 }
