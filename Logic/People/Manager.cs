c7aa89653d839e5bc3e7bdbcd6eb32783bdcaf33
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-22 15:39:55 +0200

    Update Manager.cs

--- Logic/People/Manager.cs
+++ Logic/People/Manager.cs
@@ -2,17 +2,17 @@ using System.Runtime.CompilerServices;
 
 class Manager : Person, IEmployee
 {
-    // Wat is uniek aan een manager?
-    // Managers hebben een Employee Code!
-    // Employee Code kan alleen maar gelezen worden en wordt zelf binnen de class gemaakt?
+//   Wat is uniek aan een manager?
+//   Managers hebben een Employee Code!
+//   Employee Code kan alleen maar gelezen worden en wordt zelf binnen de class gemaakt?
     private string _employeeCode = "InformaticaStinkt";
     public string EmployeeCode {get;}
-    public Manager(string firstName, string lastName, string emailAddress, string phoneNumber) : base(firstName, lastName, emailAddress, phoneNumber)
+    public Manager(string firstName, string lastName, string emailAddress, string phoneNumber, string password) : base(firstName, lastName, emailAddress, phoneNumber, password)
     {
     }
-// [1] Change the restaurant info
-// [2] Change the menu
-// [3] See all reservations
+//   [1] Change the restaurant info
+//   [2] Change the menu
+//   [3] See all reservations
 
     public void ChangeRestaurantInfo(RestaurantInfo Info, string adres, string email, string phoneNumber){
         Info.Adress = adres;
