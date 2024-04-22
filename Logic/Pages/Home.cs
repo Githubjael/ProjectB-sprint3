1464bba7759082510514aa297e44c29222db8d6f
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-22 15:43:09 +0200

    Update Home.cs

--- Logic/Pages/Home.cs
+++ Logic/Pages/Home.cs
@@ -1,7 +1,14 @@
-public static class Home 
+using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+
+public static class Home
 {
     public static string IsLoggedIn { get; private set; }
     private static string _name = "Home";
+    private static List<Person> Users = new List<Person>();
+
     public static string Name
     {
         get => _name;
@@ -17,7 +24,6 @@ public static class Home
         Console.WriteLine("[L]: Log in");
         Console.WriteLine("[S]: Sign up");
 
-
         while (true)
         {
             string UserChoice = Console.ReadLine().ToUpper();
@@ -26,10 +32,10 @@ public static class Home
             {
                 case "L":
                     LogIn();
-                    return;
+                    break;
                 case "S":
                     SignUp();
-                    return;
+                    break;
                 case "M":
                     Menu.Options();
                     return;
@@ -48,37 +54,84 @@ public static class Home
             }
         }
     }
-
     public static void SignUp()
     {
-        Console.WriteLine("Enter your first name:");
-        string firstName = Console.ReadLine();
-        
-        Console.WriteLine("Enter your last name:");
-        string lastName = Console.ReadLine();
-        
-        Console.WriteLine("Enter your email address:");
-        string email = Console.ReadLine();
-        
-        Console.WriteLine("Enter a password:");
-        string password = Console.ReadLine();
-        
+        string firstName;
+        do{
+        System.Console.WriteLine("Enter a valid first name:");
+        firstName = Console.ReadLine();
+        } while (!CheckUserInfo.IsValidName(firstName));
+
+
+        string lastName;
+        do{
+        System.Console.WriteLine("Enter a valid last name:");
+        lastName = Console.ReadLine();
+        } while (!CheckUserInfo.IsValidName(lastName));
+
+
+        string email;
+        do{
+        System.Console.WriteLine("Enter a valid email:");
+        email = Console.ReadLine();
+        } while (!CheckUserInfo.IsValidEmail(email));
+
+        string phoneNumber;
+        do{
+            Console.WriteLine("Enter a valid phone number:");
+            phoneNumber = Console.ReadLine();
+        } while (!CheckUserInfo.IsValidPhoneNumber(phoneNumber));
+
+
+        string password;
+        do{
+            Console.WriteLine("Enter a valid password:");
+            password = Console.ReadLine();
+        } while (password == "");
+
         Console.WriteLine("Confirm your password:");
         string confirmPassword = Console.ReadLine();
 
-        // Store the information in JSON
+        if (password != confirmPassword)
+        {
+            do
+            {
+                Console.WriteLine("Passwords do not match. Please try again.");
+                confirmPassword = Console.ReadLine();
+            } while  (password != confirmPassword);
+        }
+
+
+        var newUser = new Guest(firstName, lastName, email, phoneNumber, password);
+        UsersAccess.SaveUser(newUser);
+
+        Console.WriteLine("Account created successfully. You can now log in.");
     }
 
     public static void LogIn()
     {
-        Console.WriteLine("Email address:");
-        string email = Console.ReadLine();
-        
-        Console.WriteLine("Password:");
-        string password = Console.ReadLine();
-        
-        // Check with JSON
-        // If email and password match --> IsLoggedIn = true;
-        // If password doesn't match, allow the user to proceed without logging in
+        bool isLoggedIn = false;
+
+        while (!isLoggedIn)
+        {
+            Console.WriteLine("Email address:");
+            string email = Console.ReadLine();
+
+            Console.WriteLine("Password:");
+            string password = Console.ReadLine();
+
+            var user = UsersAccess.GetUser(email);
+            if (user != null && user.Password == password)
+            {
+                IsLoggedIn = "true";
+                Console.WriteLine("Logged in successfully!");
+                isLoggedIn = true; 
+            }
+            else
+            {
+                Console.WriteLine("Invalid email or password. Please try again.");
+            }
+        }
     }
+
 }
