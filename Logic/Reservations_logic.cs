7bbbefbf21d35c585940bea0cdc187773e1df693
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-04-26 16:09:25 +0200

    Update Reservations_logic.cs

--- Logic/Reservations_logic.cs
+++ Logic/Reservations_logic.cs
@@ -131,74 +131,83 @@ public class ReservationLogic
 
 
     // check of tafel bezet is
-    public static bool CheckReservedTable(string ID, string Date, string Time)
+public static bool CheckReservedTable(string ID, string Date, string Time)
+{
+    foreach (ReservationDataModel reservation in _reservation)
     {
-        foreach (ReservationDataModel reservation in _reservation)
+        foreach (var table in reservation.Tables)
         {
-            for (int i = 0; i < reservation.Tables.Count; i++)
-            { // Waarom gaat ie niet door de lijst met meer dan 1 Tafel?
-                if (reservation.Tables[i].ID == ID && reservation.Date == Date && reservation.Time == Time)
-                {
-                    return true;
-                }
+            if (table.ID == ID && reservation.Date == Date && reservation.Time == Time)
+            {
+                return true;
             }
         }
-        return false;
     }
-    // het doel van deze static function is om de table type te veranderen als er geen unreserved table
-    // beschickbaar zijn
-    public static List<Table> SwitchIfNull(Table foundnull, int type)
+    return false;
+}
+   public static List<Table> SwitchIfNull(int type)
     {
-        if (foundnull is null)
+        List<Table> tables = new List<Table>() { };
+        if (type == 2)
         {
-            List<Table> tables = new List<Table>() { };
-            if (type == 2)
+            // 6 tafels geven als ook vol
+            var found = ReservedTable.TableTracker.Find(x => x.Type == 4 && x.Reserved == false);
+            if (!(found is null))
             {
-                // 6 tafels geven als ook vol
-                var found = ReservedTable.TableTracker.Find(x => x.Type == 4 && x.Reserved == false);
+                found.IsReserved();
+                tables.Add(found);
+            }
+            else
+            {
+                found = ReservedTable.TableTracker.Find(x => x.Type == 6 && !x.Reserved);
                 if (!(found is null))
                 {
                     found.IsReserved();
                     tables.Add(found);
                 }
-                else
+            }
+        }
+        else if (type == 4)
+        {
+            var found = ReservedTable.TableTracker.Find(x => x.Type == 6 && x.Reserved == false);
+            if (found is null)
+            {
+                List<int> tabletypes = new List<int>() { 2, 2 };
+                tables = new List<Table>() { };
+                foreach (int types in tabletypes)
                 {
-                    found = ReservedTable.TableTracker.Find(x => x.Type == 6 && !x.Reserved);
+                    found = ReservedTable.TableTracker.Find(x => x.Type == types && x.Reserved == false);
                     if (!(found is null))
                     {
                         found.IsReserved();
                         tables.Add(found);
                     }
                 }
+                // ReservationDataModel Reservation = new ReservationDataModel(guestID, FirstName, LastName, PhoneNumber, EmailAddress, $"{ChosenDayFinal}/{ChosenMonthFinal}/{ChosenYear}", ChosenTime, tables);
+                // AddReservationToList(Reservation);
             }
-            else if (type == 4)
+            else
             {
-                var found = ReservedTable.TableTracker.Find(x => x.Type == 6 && x.Reserved == false);
-                if (found is null)
-                {
-                    List<int> tabletypes = new List<int>() { 2, 2 };
-                    tables = new List<Table>() { };
-                    foreach (int types in tabletypes)
-                    {
-                        found = ReservedTable.TableTracker.Find(x => x.Type == types && x.Reserved == false);
-                        if (!(found is null))
-                        {
-                            found.IsReserved();
-                            tables.Add(found);
-                        }
-                    }
-                    // ReservationDataModel Reservation = new ReservationDataModel(guestID, FirstName, LastName, PhoneNumber, EmailAddress, $"{ChosenDayFinal}/{ChosenMonthFinal}/{ChosenYear}", ChosenTime, tables);
-                    // AddReservationToList(Reservation);
-                }
-                else
+                found.IsReserved();
+                tables.Add(found);
+            }
+        }
+        else if (type == 6)
+        {
+            List<int> tabletypes = new List<int>() { 2, 4};
+            tables = new List<Table>();
+            foreach (int types in tabletypes)
+            {
+                var found = ReservedTable.TableTracker.Find(x => x.Type == types && x.Reserved == false);
+                if (!(found is null))
                 {
                     found.IsReserved();
                     tables.Add(found);
                 }
             }
-            else if (type == 6)
+            if (tables.Count != 2)
             {
-                List<int> tabletypes = new List<int>() { 2, 4};
+                tabletypes = new List<int>() { 2, 2, 2 };
                 tables = new List<Table>();
                 foreach (int types in tabletypes)
                 {
@@ -209,29 +218,14 @@ public class ReservationLogic
                         tables.Add(found);
                     }
                 }
-                if (tables.Count != 2)
-                {
-                    tabletypes = new List<int>() { 2, 2, 2 };
-                    tables = new List<Table>();
-                    foreach (int types in tabletypes)
-                    {
-                        var found = ReservedTable.TableTracker.Find(x => x.Type == types && x.Reserved == false);
-                        if (!(found is null))
-                        {
-                            found.IsReserved();
-                            tables.Add(found);
-                        }
-                    }
-                    // ReservationDataModel Reservation = new ReservationDataModel(guestID, FirstName, LastName, PhoneNumber, EmailAddress, $"{ChosenDayFinal}/{ChosenMonthFinal}/{ChosenYear}", ChosenTime, tables);
-                    // AddReservationToList(Reservation);
-                }
+                // ReservationDataModel Reservation = new ReservationDataModel(guestID, FirstName, LastName, PhoneNumber, EmailAddress, $"{ChosenDayFinal}/{ChosenMonthFinal}/{ChosenYear}", ChosenTime, tables);
+                // AddReservationToList(Reservation);
             }
-            return tables;
         }
-        else
-        {
-            Console.WriteLine("Error in adjusting tables for reservation.");
-            return new(null);
+        return tables;
         }
-    }
+
+    // het doel van deze static function is om de table type te veranderen als er geen unreserved table
+    // beschickbaar zijn
+    
 }
