a6412e8d80081e26e5dc9f22041be404022af17c
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-06 10:08:02 +0200

    Update Program.cs

--- Interactions/Program.cs
+++ Interactions/Program.cs
@@ -2,6 +2,7 @@ public class Program
 {
     public static void Main()
     {
+        ReservedTable.PopulateTables();
         ReservationLogic.AddReservationFromJson(); 
         Home.Options();
     }
