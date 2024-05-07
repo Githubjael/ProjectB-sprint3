66e7221c441b55ca2c0f2db9964ab85ffe7e8298
Githubjael <jaelimamdi@gmail.com>
2024-05-07 22:06:14 +0200

    deze ook van derived van abstract

--- Logic/Furniture/BarTable.cs
+++ Logic/Furniture/BarTable.cs
@@ -2,4 +2,12 @@ class BarTable : Table{
     public BarTable(string id, int type) : base(id + "D"){
         Type = type;
     }
+        public override void IsReserved()
+    {
+        Reserved = true;
+    }
+    public override void Cancelled()
+    {
+        Reserved = false;
+    }
 }
