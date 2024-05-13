79e8a3312e88bdac54dadcb3ad2cbc84f94829ac
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-13 10:58:21 +0200

    Update ReservationMenu.cs

--- Interactions/ReservationMenu.cs
+++ Interactions/ReservationMenu.cs
@@ -1,1 +1,36 @@
+static class ReservationMenu
+{
+    public static void Options()
+    {
+        while(true){
+        Console.WriteLine("[H]: Home");
+        Console.WriteLine("[M]: Make reservation");
+        Console.WriteLine("[CR]: Cancel reservation");
 
+            string userChoice = Console.ReadLine().ToUpper();
+
+            switch (userChoice)
+            {
+                case "M":
+                    // Make reservation
+                    Reservation.MakeReservation();
+                    Options();
+                    return;
+                case "CR":
+                    // Cancel reservation
+                    System.Console.WriteLine("Enter your guest ID"); // voorbeeld guestID deze wordt normaal ingevoerd in program maar dat zien we later wel
+                    int guestID = Convert.ToInt32(Console.ReadLine());
+                    ReservationLogic.CancelReservation(guestID);
+                    Options();
+                    return;
+                case "H":
+                    Home.Options();
+                    return;
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    Options();
+                    break;
+            }
+        }
+    }
+}
