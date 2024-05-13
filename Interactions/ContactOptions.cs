60b76f6d057f9ff5f5f40464d5fea0051f26a769
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-13 10:57:12 +0200

    Update ContactOptions.cs

--- Interactions/ContactOptions.cs
+++ Interactions/ContactOptions.cs
@@ -1,1 +1,35 @@
+static class ContactOptions
+{
+        public static void Options()
+    {
+        SeeContactInfo();
+        Console.WriteLine("[H]: Home");
+        Console.WriteLine(""); 
 
+        while (true)
+        {
+            string userChoice = Console.ReadLine().ToUpper();
+
+            switch (userChoice)
+            {
+                case "H":
+                    Home.Options();
+                    return;
+
+                case "???":
+                    // Add logic for the specific contact option
+                    return;
+
+                default:
+                    Console.WriteLine("Invalid input. Please try again.");
+                    break;
+            }
+        }
+    }
+    public static void SeeContactInfo()
+    {
+        Console.WriteLine(RestaurantInfo.Adress);
+        Console.WriteLine(RestaurantInfo.PhoneNumber);
+        Console.WriteLine(RestaurantInfo.Email);
+    }
+}
