b8047c0bdf885fa6b17cb39d8b7818c830576b55
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-13 10:57:45 +0200

    Update Messages.cs

--- Interactions/Messages.cs
+++ Interactions/Messages.cs
@@ -1,1 +1,27 @@
+static class Messages
+{
 
+    public static void Greeting()
+    {
+        if (Home.IsLoggedIn)
+            System.Console.WriteLine($"Welkom back, {Home.guestName}!");
+        else if(Home.ManagerLoggedIn)
+            System.Console.WriteLine($"Welkom, Jane");
+    }
+    public static void ReservationGreetings()
+    {
+        if(Home.IsLoggedIn)
+        {
+            System.Console.WriteLine($"Hi {Home.guestName}! please provide us with the following details:");
+        }
+        else{
+            System.Console.WriteLine("We are honored by your interest in booking at our restaurant!");
+            System.Console.WriteLine("Please answer the following questions:");
+        }  
+    }
+
+    public static void Thanking4Reservation(int GuestID)
+    {
+        System.Console.WriteLine($"Thank you for choosing to book with us! Your guest ID is {GuestID}.\nIf you need to cancel your reservation, please provide this guest ID for easy processing. We look forward to serving you!");
+    }
+}
