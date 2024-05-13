cdc165d2637ea5160261a0c84a1af5561e2e7e25
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-05-13 13:29:11 +0200

    Update RestaurantInfo.cs

--- Logic/RestaurantInfo.cs
+++ Logic/RestaurantInfo.cs
@@ -1,13 +1,13 @@
-public class RestaurantInfo{
-    public string Adress{get; set;}
-    public string Email{get; set;}
-    public string Phone_number{get; set;}
+public static class RestaurantInfo{
+    public  static string Adress{get; set;}
+    public  static string Email{get; set;}
+    public static string Phone_number{get; set;}
 
-    public RestaurantInfo(string adress, string email, string phone_number)
-    {
-        this.Adress = adress;
-        this.Email = email;
-        this.Phone_number = phone_number;
-    }
+    // public RestaurantInfo(string adress, string email, string phone_number)
+    // {
+    //     this.Adress = adress;
+    //     this.Email = email;
+    //     this.Phone_number = phone_number;
+    // }
 
 }
