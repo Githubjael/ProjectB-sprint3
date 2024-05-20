1d9954e9953833011ddc7f57aecfa2288038283f
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-20 13:35:03 +0200

    Update Contact.cs

--- Logic/Pages/Contact.cs
+++ Logic/Pages/Contact.cs
@@ -1,41 +1,7 @@
 public static class Contact 
 {
-    private static string _name = "Contact";
+    public static string Name => "Contact";
 
-    public static string Name
-    {
-        get => _name;
-    }
-
-    public static void Options()
-    {
-        Console.WriteLine("[1]: Home");
-        Console.WriteLine("[2]: Contact Information"); 
-
-        while (true)
-        {
-            string userChoice = Console.ReadLine().ToUpper();
-
-            switch (userChoice)
-            {
-                case "1":
-                    Home.Options();
-                    return;
-
-                case "2":
-                    // Add logic for the specific contact option
-                    Console.WriteLine("For further questions you could contact us with the following:\n");
-                    Console.WriteLine("Restaurant Service Phone Number: 0611223344");
-                    Console.WriteLine("Restaurant Service Email: JakesRestaurant@Gmail.com");
-
-                    Console.WriteLine("Its quite busy these days in our restaurant because of the quality and fresh food we serve, so please have patience if we take a bit longer than usual.\n\n\n");
-                    Home.Options();
-                    return;
-
-                default:
-                    Console.WriteLine("Invalid input. Please try again.");
-                    break;
-            }
-        }
-    }
+    public static void Options() => ContactOptions.Options();
+    public static void SeeContactInfo() => ContactOptions.SeeContactInfo();
 }
