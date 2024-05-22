d2b5a7e86dde4b2b12b5380735a332230301fbdf
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-05-22 09:56:02 +0200

    Update MenuItem.cs

--- Logic/Pages/MenuItem.cs
+++ Logic/Pages/MenuItem.cs
@@ -2,11 +2,15 @@ public class MenuItem{
     public string Name { get; set; }
     public double Price { get; set; }
     public string Category { get; set; }
+    
+    public List<string> Ingredients { get; }
 
-    public MenuItem(string name, double price, string category){
+    public MenuItem(string name, double price, string category, List<string> ingredients){
         Name = name;
         Price = price;
         Category = category;
+        Ingredients = ingredients;
+
     }
 //CHANGE CATEGORY
 }
