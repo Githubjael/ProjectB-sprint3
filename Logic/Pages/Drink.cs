5c70f59da4482e193a4eb5adc99b9fec298579b6
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-05-22 09:55:30 +0200

    Update Drink.cs

--- Logic/Pages/Drink.cs
+++ Logic/Pages/Drink.cs
@@ -1,1 +1,11 @@
+public class Drink
+{
+    public string Name { get; }
+    public List<string> Ingredients { get; }
 
+    public Drink(string name, List<string> ingredients)
+    {
+        Name = name;
+        Ingredients = ingredients;
+    }
+}
