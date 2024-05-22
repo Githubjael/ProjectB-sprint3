ffcfcae20949439589d2f6d09a27d2da7511fa68
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-05-22 11:27:15 +0200

    Update ReservationMenu.cs

--- Interactions/ReservationMenu.cs
+++ Interactions/ReservationMenu.cs
@@ -1,39 +1,114 @@
-static class ReservationMenu
+using System;
+using System.Collections.Generic;
+
+public static class ReservationMenu
 {
     public static void Options()
     {
-        while(true){
-        Console.WriteLine("[1]: Home");
-        Console.WriteLine("[2]: Make reservation");
-        Console.WriteLine("[3]: Cancel reservation");
+        while (true)
+        {
+            Console.WriteLine("[H]: Home");
+            Console.WriteLine("[M]: Make reservation");
+            Console.WriteLine("[CR]: Cancel reservation");
+            Console.WriteLine("[V]: View reservation history");
 
             string userChoice = Console.ReadLine().ToUpper();
 
             switch (userChoice)
             {
-                case "2":
+                case "M":
                     // Make reservation
                     Reservation.MakeReservation();
                     Options();
                     return;
-                case "3":
+                case "CR":
+                    
+
                     // Cancel reservation
-                    System.Console.WriteLine("Enter your guest ID"); // voorbeeld guestID deze wordt normaal ingevoerd in program maar dat zien we later wel
-                    try{
-                    int guestID = Convert.ToInt32(Console.ReadLine());
-                    ReservationLogic.CancelReservation(guestID);
+                    if (Home.IsLoggedIn && !Home.ManagerLoggedIn)
+                    {
+                        Console.WriteLine("Your upcoming reservations:");
+                        var now = DateTime.Now;
+                        var reservations = ReservationLogic.FindReservationsByEmail(Home.guestEmail)
+                                                            .Where(r => DateTime.Parse(r.Date + " " + r.Time) > now);
+
+                        if (reservations.Any())
+                        {
+                            foreach (var reservation in reservations)
+                            {
+                                Console.WriteLine($"Guest ID: {reservation.GuestID}");
+                                Console.WriteLine($"Name: {reservation.FirstName} {reservation.LastName}");
+                                Console.WriteLine($"Date: {reservation.Date}");
+                                Console.WriteLine($"Time: {reservation.Time}");
+                                Console.WriteLine();
+                            }
+
+                            string date;
+                            DateTime parsedDate;
+                            do
+                            {
+                                Console.WriteLine("On which date would you like to cancel your reservation? (dd-MM-yyyy)");
+                                date = Console.ReadLine();
+                            }
+                            while (!DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out parsedDate));
+
+                            string time;
+                            TimeSpan parsedTime;
+                            do
+                            {
+                                Console.WriteLine("Enter the time of your reservation: (hh:mm)");
+                                time = Console.ReadLine();
+                            }
+                            while (!TimeSpan.TryParseExact(time, "hh\\:mm", null, System.Globalization.TimeSpanStyles.None, out parsedTime));
+
+                            ReservationLogic.CancelReservationByAcc(Home.guestEmail, date, time);
+                        }
+                        else
+                        {
+                            Console.WriteLine("No upcoming reservations found.");
+                        }
                     }
-                    catch (Exception ex){
-                        System.Console.WriteLine("Please enter a valid number");
+                    else
+                    {
+                        Console.WriteLine("You need to log in to cancel a reservation.");
                     }
                     Options();
                     return;
-                case "1":
+
+
+                case "V":
+                    // View reservation history
+                    if (Home.IsLoggedIn && !Home.ManagerLoggedIn)
+                    {
+                        Console.WriteLine("Your reservation history:");
+                        var reservations = ReservationLogic.FindReservationsByEmail(Home.guestEmail);
+                        if (reservations != null && reservations.Count > 0)
+                        {
+                            foreach (var reservation in reservations)
+                            {
+                                Console.WriteLine($"Guest ID: {reservation.GuestID}");
+                                Console.WriteLine($"Name: {reservation.FirstName} {reservation.LastName}");
+                                Console.WriteLine($"Date: {reservation.Date}");
+                                Console.WriteLine($"Time: {reservation.Time}");
+                                Console.WriteLine();
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("No reservations found.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("You need to log in to view your reservation history.");
+                    }
+                    Options();
+                    return;
+                case "H":
                     Home.Options();
                     return;
                 default:
                     Console.WriteLine("Invalid input. Please try again.");
-                    Options();
                     break;
             }
         }
