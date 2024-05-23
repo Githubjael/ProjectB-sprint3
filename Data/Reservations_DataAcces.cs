6d68138677939495b01a908c8163ad8128c9f5bf
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:05:43 +0200

    Update Reservations_DataAcces.cs

--- Data/Reservations_DataAcces.cs
+++ Data/Reservations_DataAcces.cs
@@ -2,26 +2,7 @@ using Newtonsoft.Json;
 
 class ReservationDataAccess
 {
-    public static string fileName = @"..\..\..\DataSources\Reservations.Json";
-
-    // voor nu schrijven we alle reservations naar json
-    // er wordt nog niet nagekeken of de dagen/maanden/tafels etc etc available zijn 
-    // en reservations kunnen nog niet worden geannuleerd
-    public static List<ReservationDataModel> ReadFromJson() // Alle reserveringen lezen en in een lijst stoppen
-    {
-        using StreamReader reader = new(fileName);
-        var json = reader.ReadToEnd();
-        List<ReservationDataModel> AllReservations = JsonConvert.DeserializeObject<List<ReservationDataModel>>(json);
-        return AllReservations;
-        // in logische laag moet je kijken of er een tafelID is
-    }
-    public static void WriteToJson(List<ReservationDataModel> ReservationList)
-    {
-        // write to json
-        StreamWriter writer = new(fileName);
-        string List2Json = JsonConvert.SerializeObject(ReservationList, Formatting.Indented);
-        writer.Write(List2Json);
-        writer.Close();
-        // write to json
-    }
+    public static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Reservations.Json";
+    public static List<ReservationDataModel> ReadFromJson() => JsonStuff.ReadFromJson<ReservationDataModel>(fileName);
+    public static void WriteToJson(List<ReservationDataModel> ReservationList) => JsonStuff.WriteToJson(ReservationList, fileName);
 }
