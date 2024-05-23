48ea68b73e404fe8971fcb2d30e28223b6aec70e
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:03:23 +0200

    Update ContactAccess.cs

--- Data/ContactAccess.cs
+++ Data/ContactAccess.cs
@@ -1,1 +1,6 @@
-
+class ContactAccess
+{
+    public static string Filename = @"C:\Users\User\Desktop\Project B\DataSources\Contact.Json";
+    public static List<ContactDataModel> ReadFromJson() => JsonStuff.ReadFromJson<ContactDataModel>(Filename);
+    public static void WriteToJson(List<ContactDataModel> ContactInfo) => JsonStuff.WriteToJson(ContactInfo, Filename);
+}
