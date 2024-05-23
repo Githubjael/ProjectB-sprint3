3a6de87c7c20e0f7415b1f701ec660f8c50dbf00
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:06:05 +0200

    Update ReviewAccess.cs

--- Data/ReviewAccess.cs
+++ Data/ReviewAccess.cs
@@ -2,40 +2,7 @@ using Newtonsoft.Json;
 
 class ReviewAccess
 {
-    private static string fileName = @"..\..\..\DataSources\Reviews.Json"; //vergeet niet je eigen path te copy
-
-    public static List<Review> ReadFromJson()
-    {
-        try
-        {
-            using (StreamReader reader = new StreamReader(fileName))
-            {
-                string json = reader.ReadToEnd();
-                List<Review> allreviews = JsonConvert.DeserializeObject<List<Review>>(json);
-                return allreviews;
-            }
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("no json file found");
-            return new List<Review>();
-        }
-    }
-
-    public static void WriteToJson(List<Review> reviews)
-    {
-        try
-        {
-            using (StreamWriter writer = new StreamWriter(fileName))
-            {
-                string json = JsonConvert.SerializeObject(reviews);
-                writer.Write(json);
-            }
-        }
-        catch (IOException ex)
-        {
-            Console.WriteLine($"Error writing reviews file: {ex.Message}");
-            throw;
-        }
-    }
+    private static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Reviews.Json"; //vergeet niet je eigen path te copy
+    public static List<Review> ReadFromJson() => JsonStuff.ReadFromJson<Review>(fileName);
+    public static void WriteToJson(List<Review> reviews) => JsonStuff.WriteToJson(reviews, fileName);
 }
