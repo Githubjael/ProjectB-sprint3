3b1bb79c73ff942fad0e4b3dcfec620ca7275bc4
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:09:03 +0200

    Delete Logic/RestaurantInfo.cs

--- Logic/RestaurantInfo.cs
+++
@@ -1,6 +0,0 @@
-public class RestaurantInfo{
-    public static string Adress{get; set;} = "Wijnhaven 30";
-    public static string Email{get; set;} = "JakesRestaurant@gmail.com";
-    public static string PhoneNumber{get; set;} = "06 112 233 44";
-
-}
