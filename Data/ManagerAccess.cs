2145a17a825539b3727c6267500007e5514e76ca
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:05:17 +0200

    Update ManagerAccess.cs

--- Data/ManagerAccess.cs
+++ Data/ManagerAccess.cs
@@ -4,21 +4,5 @@ class ManagerAccess
 {
     private static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Manager.Json"; //vergeet niet je eigen path te copy
 
-    public static List<Manager> ReadFromJson()
-    {
-        try
-        {
-            using (StreamReader reader = new StreamReader(fileName))
-            {
-                string json = reader.ReadToEnd();
-                List<Manager> allusers = JsonConvert.DeserializeObject<List<Manager>>(json);
-                return allusers;
-            }
-        }
-        catch (FileNotFoundException)
-        {
-            Console.WriteLine("no json file found");
-            return new List<Manager>();
-        }
-    }
+    public static List<Manager> ReadFromJson() => JsonStuff.ReadFromJson<Manager>(fileName);
 }
