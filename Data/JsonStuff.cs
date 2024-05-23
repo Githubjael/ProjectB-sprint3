589753dba9ff6c42525dc1a4b8fe7aa2a56f0191
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-05-23 19:04:14 +0200

    Update JsonStuff.cs (GENERICS FOR ALL ACCESS FILES)

--- Data/JsonStuff.cs
+++ Data/JsonStuff.cs
@@ -1,1 +1,23 @@
+using Newtonsoft.Json;
+class JsonStuff
+{
+    public static List<T> ReadFromJson<T>(string FileName) // Alle reserveringen lezen en in een lijst stoppen
+    {
+        using StreamReader reader = new(FileName);
+        var json = reader.ReadToEnd();
+        List<T> GenericList = JsonConvert.DeserializeObject<List<T>>(json);
+        return GenericList;
+        // in logische laag moet je kijken of er een tafelID is
+    }
 
+
+    public static void WriteToJson<T>(List<T> ReservationList, string fileName)
+    {
+        // write to json
+        StreamWriter writer = new(fileName);
+        string List2Json = JsonConvert.SerializeObject(ReservationList, Formatting.Indented);
+        writer.Write(List2Json);
+        writer.Close();
+        // write to json
+    }
+}
