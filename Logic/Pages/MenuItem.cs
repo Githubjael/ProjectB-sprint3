fefb4f3a18ea13fd311b5c1872cda35e1c207647
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-05-27 15:53:21 +0200

    Update MenuItem.cs

--- Logic/Pages/MenuItem.cs
+++ Logic/Pages/MenuItem.cs
@@ -1,16 +1,17 @@
 public class MenuItem{
+    public int Id { get; set; }
     public string Name { get; set; }
     public double Price { get; set; }
     public string Category { get; set; }
     public string Symbol { get; set; }
     public List<string> Ingredients { get; }
 
-    public MenuItem(string name, double price, string category, List<string> ingredients, string symbol){
+    public MenuItem(int id, string name, double price, string category, List<string> ingredients, string symbol){
+        Id = id;
         Name = name;
         Price = price;
         Category = category;
         Ingredients = ingredients;
         Symbol = symbol;
-
     }
 }
