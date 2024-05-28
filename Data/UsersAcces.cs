511f2e935b7fafb5b7aeff827f09f18d9228572a
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-05-28 23:15:40 +0200

    Update UsersAcces.cs

--- Data/UsersAcces.cs
+++ Data/UsersAcces.cs
@@ -1,8 +1,10 @@
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
 
 class UsersAccess
 {
-    private static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Users.Json"; //vergeet niet je eigen path te copy
+    private static string fileName = @"C:\Users\Hilal\OneDrive\Bureaublad\eindelijk\ProjectB-sprint3-main\DataSources\users.json"; // copy je eige path.
 
     public static List<Guest> ReadFromJson() => JsonStuff.ReadFromJson<Guest>(fileName);
 
@@ -11,7 +13,7 @@ class UsersAccess
     public static void SaveUser(Guest user)
     {
         var users = LoadUsers();
-        users.Add(user); 
+        users.Add(user);
         SaveUsers(users);
     }
 
@@ -28,16 +30,34 @@ class UsersAccess
         }
     }
 
-
-
     public static void SaveUsers(List<Guest> users)
     {
-        WriteToJson(users);
+        using (StreamWriter file = File.CreateText(fileName))
+        {
+            var serializer = new JsonSerializer();
+            serializer.Formatting = Formatting.Indented;
+            serializer.Serialize(file, users);
+        }
     }
 
     public static Guest GetUser(string email)
     {
         var users = LoadUsers();
         return users.Find(u => u.EmailAddress == email);
+    }
+
+    public static void UpdateUser(Guest updatedUser)
+    {
+        var users = LoadUsers();
+        var index = users.FindIndex(u => u.EmailAddress == updatedUser.EmailAddress);
+        if (index != -1)
+        {
+            users[index] = updatedUser;
+            SaveUsers(users);
+        }
+        else
+        {
+            Console.WriteLine($"User {updatedUser.EmailAddress} not found.");
+        }
     }
 }
