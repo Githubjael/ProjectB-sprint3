ffdf6df0ebf338ba43ca0dd9c88a9e9d67b663e7
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-06-05 18:09:04 +0200

    Create PreOrder.cs

---
+++ Data/PreOrder.cs
@@ -0,0 +1,24 @@
+using System.Text.Json.Serialization;
+class PreOrder
+{
+    [JsonPropertyName("guest ID")]
+    public string GuestID {get; set;}
+    [JsonPropertyName("Order")]
+    public List<string> Dishes {get; set;}
+    [JsonPropertyName("Time")]
+    public TimeOnly Time{get; set;}
+
+    [JsonPropertyName("Date")]
+    public DateOnly Date{get; set;}
+
+    [JsonConstructor]
+    public PreOrder(string guestID, List<string> dishes, string date, string time)
+    {
+        GuestID = guestID;
+        Dishes = dishes;
+        // Time only om te sorteren op basis vantijd. Use LINQ ORDERBY
+        Time = TimeOnly.Parse(time);
+        Date = DateOnly.Parse(date);
+    }
+
+}
