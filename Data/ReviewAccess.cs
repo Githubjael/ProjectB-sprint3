9b4a3c6155380929a64f97b04cc6b7c140449f14
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-11 21:54:48 +0200

    Update ReviewAccess.cs

--- Data/ReviewAccess.cs
+++ Data/ReviewAccess.cs
@@ -2,7 +2,7 @@ using Newtonsoft.Json;
 
 class ReviewAccess
 {
-    private static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Reviews.Json"; //vergeet niet je eigen path te copy
+    private static string fileName = @"..\..\..\DataSources\Reviews.Json"; //vergeet niet je eigen path te copy
     public static List<Review> ReadFromJson() => JsonStuff.ReadFromJson<Review>(fileName);
     public static void WriteToJson(List<Review> reviews) => JsonStuff.WriteToJson(reviews, fileName);
 }
