1d0bcc650a6aceb4189745e14090e652f38d66e4
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-11 21:54:06 +0200

    Update Reservations_DataAcces.cs
    updated filepath

--- Data/Reservations_DataAcces.cs
+++ Data/Reservations_DataAcces.cs
@@ -2,7 +2,7 @@ using Newtonsoft.Json;
 
 class ReservationDataAccess
 {
-    public static string fileName = @"C:\Users\User\Desktop\Project B\DataSources\Reservations.Json";
+    public static string fileName = @"..\..\..\DataSources\Reservations.Json";
     public static List<ReservationDataModel> ReadFromJson() => JsonStuff.ReadFromJson<ReservationDataModel>(fileName);
     public static void WriteToJson(List<ReservationDataModel> ReservationList) => JsonStuff.WriteToJson(ReservationList, fileName);
 }
