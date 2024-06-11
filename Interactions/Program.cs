d809f3b702b0940b7821a0113ae5e00409f77137
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-11 11:21:34 +0200

    Update Program.cs

--- Interactions/Program.cs
+++ Interactions/Program.cs
@@ -1,7 +1,10 @@
+using System;
+using System.Text;
 public class Program
 {
     public static void Main()
     {
+        Console.OutputEncoding = Encoding.UTF8;
         ReservedTable.PopulateTables();
         ReservationLogic.AddReservationFromJson(); 
         Home.Options();
