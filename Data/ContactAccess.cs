36719adbdf32f6db9df7113bfc298c9708d230ab
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-12 11:38:31 +0200

    Update ContactAccess.cs

--- Data/ContactAccess.cs
+++ Data/ContactAccess.cs
@@ -1,6 +1,6 @@
 class ContactAccess
 {
-    public static string Filename = @"..\..\..\DataSources\Contact.json";
+    private static string Filename = @"..\..\..\DataSources\Contact.json";
     public static List<ContactDataModel> ReadFromJson() => JsonStuff.ReadFromJson<ContactDataModel>(Filename);
     public static void WriteToJson(List<ContactDataModel> ContactInfo) => JsonStuff.WriteToJson(ContactInfo, Filename);
 }
