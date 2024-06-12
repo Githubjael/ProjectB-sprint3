c356fb08983993f3d0c8a508b007eecb95e7121e
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-06-12 09:37:34 +0200

    Update PreOrdering.cs

--- Logic/PreOrdering.cs
+++ Logic/PreOrdering.cs
@@ -1,8 +1,8 @@
 static class PreOrdering
 {
-    public static List<MenuItem> menuItems = Menu.MenuItems;
+    public static List<MenuItem> menuItems = Menu.menuItems;
 
-    public static void AskDish(string GuestID, string Date, string Time) => PreOrderOptions.AskDish(GuestID, Date, Time);
+    public static PreOrder AskDish(string GuestID, string Date, string Time) => PreOrderOptions.AskDish(GuestID, Date, Time);
     public static void CancelPreOrder() => PreOrderOptions.CancelPreOrder();
 
     public static void ViewPreOrders() => PreOrderOptions.ViewPreOrders();
