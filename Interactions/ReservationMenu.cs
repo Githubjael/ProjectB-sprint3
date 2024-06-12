06c1ffc3e8bb2289c806578618ff05cbd6244291
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-06-12 09:41:11 +0200

    Update ReservationMenu.cs

--- Interactions/ReservationMenu.cs
+++ Interactions/ReservationMenu.cs
@@ -88,6 +88,12 @@ public static class ReservationMenu
                                 Console.WriteLine($"Name: {reservation.FirstName} {reservation.LastName}");
                                 Console.WriteLine($"Date: {reservation.Date}");
                                 Console.WriteLine($"Time: {reservation.Time}");
+                                System.Console.WriteLine($"Pre-order:");
+                                foreach(var dish in reservation.Preorder.Dishes)
+                                {
+                                    System.Console.WriteLine(dish);
+                                    System.Console.WriteLine(PreOrdering.menuItems.Find(x => x.Name == dish).Price);
+                                }
                                 Console.WriteLine();
                             }
                         }
