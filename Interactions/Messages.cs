23bbda35379694aec6b2cbf3365b92c2e80a2c12
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-13 14:30:12 +0200

    Update Messages.cs

--- Interactions/Messages.cs
+++ Interactions/Messages.cs
@@ -4,9 +4,9 @@ static class Messages
     public static void Greeting()
     {
         if (Home.IsLoggedIn)
-            System.Console.WriteLine($"Welkom back, {Home.guestName}!");
+            System.Console.WriteLine($"Welcome back, {Home.guestName}!");
         else if(Home.ManagerLoggedIn)
-            System.Console.WriteLine($"Welkom, Jane");
+            System.Console.WriteLine($"Welcome, Jane");
     }
     public static void ReservationGreetings()
     {
@@ -17,18 +17,18 @@ static class Messages
         }
         else{
             System.Console.WriteLine("You can Enter 'Q' to go back to the Home page at any time\n");
-            System.Threading.Thread.Sleep(1500);
+            System.Threading.Thread.Sleep(1000);
             System.Console.WriteLine("We are honored by your interest in booking at our restaurant!\n");
-            System.Threading.Thread.Sleep(1500);
+            System.Threading.Thread.Sleep(1000);
             System.Console.WriteLine("The amount of guests determines the type of your reservation.\n");
-            System.Threading.Thread.Sleep(2500);
+            System.Threading.Thread.Sleep(1000);
             System.Console.WriteLine("A reservation of 1, reserves a seat at the bar.");
             System.Console.WriteLine("A reservation of 2, reserves a 2 person table.");
             System.Console.WriteLine("A reservation of 3-4, reserves a 4 person table.");
             System.Console.WriteLine("A reservation of 5-6, reserves a 6 person table.\n");
-            System.Threading.Thread.Sleep(2500);
+            System.Threading.Thread.Sleep(2000);
             System.Console.WriteLine("Please answer the following questions:\n");
-            System.Threading.Thread.Sleep(2500);
+            System.Threading.Thread.Sleep(500);
         }  
     }
 
