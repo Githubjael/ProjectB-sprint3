7dd19ea72028dafdc72bbfb0b84831e36d16e285
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-14 20:25:38 +0200

    Update HomeOptions.cs

--- Interactions/HomeOptions.cs
+++ Interactions/HomeOptions.cs
@@ -30,7 +30,7 @@ static class HomeOptions
         Console.WriteLine("[1]: Menu");
         Console.WriteLine("[2]: Reservation");
         Console.WriteLine("[3]: Review");
-        Console.WriteLine("[4]: Contact");
+        Console.WriteLine("[4]: Info"); //in de code heet het contact
         if (Home.IsLoggedIn || Home.ManagerLoggedIn){
             Console.WriteLine("[5]: Log Out");
             Console.WriteLine("[6]: Change Password");
