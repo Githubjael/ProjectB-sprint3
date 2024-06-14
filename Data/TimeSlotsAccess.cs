e93c1e708630e44d8ad94b1f2b94821dce545e86
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-06-14 17:44:41 +0200

    Delete Data/TimeSlotsAccess.cs

--- Data/TimeSlotsAccess.cs
+++
@@ -1,6 +0,0 @@
-class TimeSlots
-{
-    public static string Filename = @"..\..\..\DataSources\TimeSlots.Json";
-    public static List<string> ReadFromJson() => JsonStuff.ReadFromJson<string>(Filename);
-    public static void WriteToJson(List<string> timeSlots) => JsonStuff.WriteToJson(timeSlots, Filename);
-}
