3b013970676b767d80c48289fbada5de7f9e7086
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-06-26 11:49:51 +0200

    Update CheckReservationInfo.cs

--- Interactions/CheckReservationInfo.cs
+++ Interactions/CheckReservationInfo.cs
@@ -1,83 +1,98 @@
 using System.Text.RegularExpressions;
-class CheckReservationInfo
+
+
+public static class CheckReservationInfo
 {
-    // Check of alle characters in voornaam letter zijn
-    public static bool CheckFirstName(string FirstName)
+    public static bool CheckFirstName(string firstName)
     {
-        if (string.IsNullOrEmpty(FirstName))
+        if (string.IsNullOrEmpty(firstName))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
-        if (FirstName.Length < 2 || FirstName.Length > 20){
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your first name needs to be 2-20 characters."); Console.ResetColor();
+        if (firstName.Length < 2 || firstName.Length > 20)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Your first name needs to be 2-20 characters.");
+            Console.ResetColor();
             return false;
         }
-        foreach(char letter in FirstName)
+        foreach (char letter in firstName)
         {
-            if (!Char.IsLetter(letter))
+            if (!char.IsLetter(letter))
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your first name must only contain letters."); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*Your first name must only contain letters.");
+                Console.ResetColor();
                 return false;
             }
         }
         return true;
     }
 
-    // check of alle characters in achternaam letters zijn
-    public static bool CheckLastName(string LastName)
+    public static bool CheckLastName(string lastName)
     {
-        if (string.IsNullOrEmpty(LastName))
+        if (string.IsNullOrEmpty(lastName))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
-        if (LastName.Length < 2 || LastName.Length > 20){
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your last name needs to be 2-20 characters."); Console.ResetColor();
+        if (lastName.Length < 2 || lastName.Length > 20)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Your last name needs to be 2-20 characters.");
+            Console.ResetColor();
             return false;
         }
-        foreach(char letter in LastName)
+        foreach (char letter in lastName)
         {
-            if (!Char.IsLetter(letter))
+            if (!char.IsLetter(letter) && !char.IsWhiteSpace(letter))
             {
-                if (!Char.IsWhiteSpace(letter)){
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your last name must only contain letters."); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*Your last name must only contain letters.");
+                Console.ResetColor();
                 return false;
-                }
             }
         }
         return true;
     }
 
-    // check of alle characters in telefoonnummer nummers zijn
-    // telefoon nummer moet niet kleiner of groter zijn dan 10
-    public static bool CheckPhoneNumber(string PhoneNumber)
+    public static bool CheckPhoneNumber(string phoneNumber)
     {
-        if (string.IsNullOrEmpty(PhoneNumber))
+        if (string.IsNullOrEmpty(phoneNumber))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
-        foreach(char number in PhoneNumber)
+        foreach (char number in phoneNumber)
         {
-            if (!Char.IsNumber(number))
+            if (!char.IsNumber(number))
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your phone number must only contain numbers."); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("*Your phone number must only contain numbers.");
+                Console.ResetColor();
                 return false;
             }
         }
-        if (PhoneNumber.Length != 10)
+        if (phoneNumber.Length != 10)
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Your phone number must contain 10 numbers."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Your phone number must contain 10 numbers.");
+            Console.ResetColor();
             return false;
         }
         return true;
     }
 
-
-    public static bool CheckEmail(string EmailAddress)
+    public static bool CheckEmail(string emailAddress)
     {
-        if (string.IsNullOrEmpty(EmailAddress))
+        if (string.IsNullOrEmpty(emailAddress))
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("*Please fill something in.");
@@ -85,10 +100,9 @@ class CheckReservationInfo
             return false;
         }
 
-        
-        string regex = @"^[^@\s]+@(?:yahoo|gmail|hotmail|outlook|live|aol|icloud)\.(com|net|org|gov|hr|nl|be|en)$";
-        
-        if (Regex.IsMatch(EmailAddress, regex, RegexOptions.IgnoreCase))
+        string regex = @"^[^@\s]+@(?:yahoo|gmail|hotmail|outlook|live|aol|icloud|hr)\.(com|net|org|gov|hr|nl|be|en)$";
+
+        if (Regex.IsMatch(emailAddress, regex, RegexOptions.IgnoreCase))
         {
             return true;
         }
@@ -101,141 +115,119 @@ class CheckReservationInfo
         }
     }
 
-
-public static bool CheckDate(string date, List<string> FullyBookedDates)
+    public static bool CheckDate(string date, List<string> fullyBookedDates)
     {
-        if (FullyBookedDates.Contains(date))
+        if (fullyBookedDates.Contains(date))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("The date you're interested in is fully booked."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The date you're interested in is fully booked.");
+            Console.ResetColor();
             return false;
         }
         if (string.IsNullOrEmpty(date))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
         foreach (char c in date)
         {
             if (!char.IsDigit(c) && c != '-')
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid characters detected. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid characters detected. Please provide the date in the format: dd-MM-yyyy.");
+                Console.ResetColor();
                 return false;
             }
         }
 
-        // Attempt to parse the date string into a DateTime object
         if (DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
         {
+            if (parsedDate.Date < DateTime.Today || ReservedTable.GetTimes(date).Count == 0)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Please provide a future date.");
+                Console.ResetColor();
+                return false;
+            }
+            return true;
+        }
+        else
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy.");
+            Console.ResetColor();
+            return false;
+        }
+    }
+
+    public static bool CheckOtherDates(string date)
+    {
         if (string.IsNullOrEmpty(date))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
         foreach (char c in date)
         {
             if (!char.IsDigit(c) && c != '-')
             {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid characters detected. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy.");
+                Console.ResetColor();
                 return false;
             }
         }
-            // Check if the parsed date is in the past
-            if (parsedDate.Date < DateTime.Today || ReservedTable.GetTimes(date).Count == 0)
-            {
-                Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Please provide a future date."); Console.ResetColor();
-                return false;
-            }
-            // Date is valid
+
+        if (DateTime.TryParseExact(date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
+        {
             return true;
         }
         else
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy.");
+            Console.ResetColor();
             return false;
         }
     }
 
-    public static bool CheckOtherDates(string Date)
-    {
-        if (string.IsNullOrEmpty(Date))
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
-            return false;
-        }
-        foreach (char c in Date)
-        {
-            if (!char.IsDigit(c) && c != '-')
-            {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
-                return false;
-            }
-        }
-
-        // Attempt to parse the Date string into a DateTime object
-        if (DateTime.TryParseExact(Date, "dd-MM-yyyy", null, System.Globalization.DateTimeStyles.None, out DateTime parsedDate))
-        {
-        foreach (char c in Date)
-        {
-            if (!char.IsDigit(c) && c != '-')
-            {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
-                return false;
-            }
-        }
-            // Date is valid
-            return true;
-        }
-        else
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("Invalid date format. Please provide the date in the format: dd-MM-yyyy."); Console.ResetColor();
-            return false;
-        }
-    }
-
-
-    public static bool CheckTimeSlot(string answer, List<string> TimeSlots)
+    public static bool CheckTimeSlot(string answer, List<string> timeSlots)
     {
         if (string.IsNullOrEmpty(answer))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill a number in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill a number in.");
+            Console.ResetColor();
             return false;
         }
-        try
+        if (!int.TryParse(answer, out int slotNumber) || slotNumber <= 0 || slotNumber > timeSlots.Count)
         {
-            Convert.ToInt32(answer);
-        }
-        catch (Exception)
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"*'{answer}' is an invalid number. Please fill a valid number in"); Console.ResetColor();
-            return false;
-        }
-        if (Convert.ToInt32(answer) > TimeSlots.Count)
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"*Number should be below {TimeSlots.Count}"); Console.ResetColor();
-            return false;
-        }
-        else if (Convert.ToInt32(answer) < 0)
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Only enter positive numbers please."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"*Number should be between 1 and {timeSlots.Count}.");
+            Console.ResetColor();
             return false;
         }
         return true;
     }
 
-    public static bool CheckGuests(string Guests) // Later komt nog nakijken op max amount of Guests yk
+    public static bool CheckGuests(string guests)
     {
-        if (string.IsNullOrEmpty(Guests))
+        if (string.IsNullOrEmpty(guests))
         {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine("*Please fill something in."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("*Please fill something in.");
+            Console.ResetColor();
             return false;
         }
-        try
+        if (!int.TryParse(guests, out _))
         {
-            Convert.ToInt32(Guests);
-        }
-        catch (Exception)
-        {
-            Console.ForegroundColor = ConsoleColor.Red; Console.WriteLine($"'{Guests}' is an invalid number."); Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"'{guests}' is an invalid number.");
+            Console.ResetColor();
             return false;
         }
         return true;
