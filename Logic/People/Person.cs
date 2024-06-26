800d183e2d3253c2d24aea800deb3c928eec2f3c
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-06-26 21:19:37 +0200

    Update Person.cs

--- Logic/People/Person.cs
+++ Logic/People/Person.cs
@@ -1,5 +1,14 @@
 public abstract class Person
 {
+    protected Person(string firstName, string lastName, string emailAddress, string phoneNumber, string password)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+        PhoneNumber = phoneNumber;
+        Password = password;
+    }
+
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string EmailAddress { get; set; }
