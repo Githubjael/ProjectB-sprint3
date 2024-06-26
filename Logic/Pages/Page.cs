e7b9f0486c102fc9701cf3541f79d304662aeda5
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-06-26 21:25:44 +0200

    Update Page.cs

--- Logic/Pages/Page.cs
+++ Logic/Pages/Page.cs
@@ -1,56 +1,61 @@
-public abstract class Page
-{
-    protected List<string> _pages = new(){
-        "[1]: Home",
-        "[2]: Menu",
-        "[3]: Reservation",
-        "[4]: Review",
-        "[5]: Contact"
-    };
-  
-    public abstract string Name { get;}
+//DEZE CLASS IS ONNODIG EN ZAL DUS GEWIST WORDEN.
 
-    public virtual void Options()
-    {
-        System.Console.WriteLine("Options:");
-        foreach(string page in _pages)
-        {
-            switch(Name){
-                case "1":
-                if (page != _pages[0])
-                {
-                    System.Console.WriteLine(page);
-                }
-                break;
-                case "2":
-                if (page != _pages[1])
-                {
-                    System.Console.WriteLine(page);
-                }
-                break;
-                case "3":
-                if (page != _pages[2])
-                {
-                    System.Console.WriteLine(page);
-                }
-                break;
-                case "4":
-                if(page != _pages[3])
-                {
-                    System.Console.WriteLine(page);
-                }
-                break;
-                case "5":
-                if (page != _pages[4])
-                {
-                    System.Console.WriteLine(page);
-                }
-                break;
-            }
-        }
-        System.Console.WriteLine("[E]: Exit");
-    }
-    // Inhoud is uniek aan de derived Pagina's
-    public abstract void Contents();
+
+
+
+//public abstract class Page
+// {
+//     protected List<string> _pages = new(){
+//         "[1]: Home",
+//         "[2]: Menu",
+//         "[3]: Reservation",
+//         "[4]: Review",
+//         "[5]: Contact"
+//     };
+  
+//     public abstract string Name { get;}
+
+//     public virtual void Options()
+//     {
+//         System.Console.WriteLine("Options:");
+//         foreach(string page in _pages)
+//         {
+//             switch(Name){
+//                 case "1":
+//                 if (page != _pages[0])
+//                 {
+//                     System.Console.WriteLine(page);
+//                 }
+//                 break;
+//                 case "2":
+//                 if (page != _pages[1])
+//                 {
+//                     System.Console.WriteLine(page);
+//                 }
+//                 break;
+//                 case "3":
+//                 if (page != _pages[2])
+//                 {
+//                     System.Console.WriteLine(page);
+//                 }
+//                 break;
+//                 case "4":
+//                 if(page != _pages[3])
+//                 {
+//                     System.Console.WriteLine(page);
+//                 }
+//                 break;
+//                 case "5":
+//                 if (page != _pages[4])
+//                 {
+//                     System.Console.WriteLine(page);
+//                 }
+//                 break;
+//             }
+//         }
+//         System.Console.WriteLine("[E]: Exit");
+//     }
+//     // Inhoud is uniek aan de derived Pagina's
+//     public abstract void Contents();
 
 }
