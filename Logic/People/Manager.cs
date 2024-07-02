5dc35cebd8d696f768778ad37ee0856ee70c6dd7
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-07-02 15:00:57 +0200

    Update Manager.cs

--- Logic/People/Manager.cs
+++ Logic/People/Manager.cs
@@ -3,8 +3,10 @@ using System.Formats.Asn1;
 
 class Manager : Person
 {
-    public string EmployeeCode { get; set; } = "AppelTaart";
+    protected internal string EmployeeCode { get; } = "AppelTaart";
+
     public Manager(string firstName, string lastName, string emailAddress, string phoneNumber, string password) :
     base(firstName, lastName, emailAddress, phoneNumber, password){}
+
 }
 
