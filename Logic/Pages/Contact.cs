bf902fd447d12d2af4021b66339fcb68d92c442c
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-07-03 17:26:36 +0200

    Update Contact.cs

--- Logic/Pages/Contact.cs
+++ Logic/Pages/Contact.cs
@@ -1,6 +1,5 @@
 public static class Contact 
 {
-    public static string Name => "Contact";
 
     public static void Options() => ContactOptions.Options();
     public static void SeeContactInfo() => ContactOptions.SeeContactInfo();
